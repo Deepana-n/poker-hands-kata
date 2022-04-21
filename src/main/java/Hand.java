f41b69637175a97cc407d249c90f6e531d1b9436
Deepana Naidu <deepana.naidu@gmail.com>
2022-04-21 23:23:13 +0100

    Added isPair() method and corresponding test method

--- src/main/java/Hand.java
+++ src/main/java/Hand.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Hand {
 
-    public static enum handRank{
+    public enum handRank{
         HIGHCARD, PAIR, TWOPAIR, THREEOFAKIND, STRAIGHT, FLUSH,
         FULLHOUSE, FOUROFAKIND, STRAIGHTFLUSH
     }
@@ -31,6 +31,19 @@ public class Hand {
 
     public String getPlayerName() {
         return playerName;
+    }
+
+    public boolean isPair(Card[] currentHand){
+        //Pair: 2 of the 5 cards in the hand have the same value.
+        Card card1 = currentHand[0];
+        Card card2 = currentHand[1];
+        Card card3 = currentHand[2];
+        Card card4 = currentHand[3];
+        Card card5 = currentHand[4];
+        return card1.equals(card2) && !card2.equals(card3) ||
+                (card2.equals(card3) && !card3.equals(card4) && !card2.equals(card1)) ||
+                (card3.equals(card4) && !card4.equals(card5) && !card3.equals(card1)) ||
+                card4.equals(card5) && !card4.equals(card3);
     }
 
 }
--- src/test/java/HandTest.java
+++ src/test/java/HandTest.java
@@ -1,5 +1,16 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class HandTest {
+    @Test
+    public void testIsPair(){
+        Hand handObj = new Hand();;
+        Card[] currentHand = {new Card("2C"),new Card("4D"),new Card("3C"),new Card("5C"),new Card("2D")};
+        Arrays.sort(currentHand);
+        assertTrue(handObj.isPair(currentHand));
+    }
 
 }
